7414c7fffd611a813e07b99c995ea96775a78c6d
Alexander Clark <alexanderclark@Alexanders-MacBook-Air.local>
2019-07-17 11:40:09 -0400

    add tests and functionality to write voltage data to csv

--- python/write_voltage_data.py
+++ python/write_voltage_data.py
@@ -2,15 +2,26 @@ import paci_2018
 import protocols
 import numpy as np
 import protocols
+import matplotlib.pyplot as plt
+import csv
 
 def save_data_to_file(model_trace):
-    model_data = np.array([model_trace.t, model_trace.y]) 
+    model_data = np.transpose(np.array([
+        np.asarray(model_trace.t), 
+        np.asarray(model_trace.y)]))
     
     np.savetxt('./data/paci_sap_baseline.csv', model_data, delimiter=',')
+
+def load_and_plot_data(path_to_output):
+    trace_data = np.loadtxt(path_to_output, delimiter=',')
+
+    plt.plot(trace_data[:,0], trace_data[:,1])
+    plt.savefig('./data/paci_sap_baseline.png')
 
 def main():
     protocol = protocols.SingleActionPotentialProtocol()
     save_data_to_file(paci_2018.generate_trace(protocol))
+    load_and_plot_data('./data/paci_sap_baseline.csv')
 
 
 if __name__ == '__main__':
--- python/write_voltage_data_test.py
+++ python/write_voltage_data_test.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 import paci_2018
 import numpy.testing as tst
+import write_voltage_data
 
 class TestWritingData(unittest.TestCase):
     def test_file_exists(self):
@@ -16,13 +17,28 @@ class TestWritingData(unittest.TestCase):
     def test_write_data(self):
         path_to_output = './data/paci_sap_baseline.csv'
         protocol = protocols.SingleActionPotentialProtocol()
-        
         baseline_trace = paci_2018.generate_trace(protocol)
+
+        write_voltage_data.save_data_to_file(baseline_trace)
         with open(path_to_output) as csv_file:
             csv_reader = csv.reader(csv_file)
             loaded_data = np.array(list(csv_reader)) 
+
+        print(len(loaded_data))
 
         tst.assert_raises(AssertionError, tst.assert_array_equal, \
                 baseline_trace.y, loaded_data[:,1], \
                 'loaded data is equal to Paci')
 
+    def test_load_and_plot_data(self):
+        path_to_data = './data/paci_sap_baseline.csv'
+        path_to_figure = './data/paci_sap_baseline.png' 
+        try:
+            os.remove(path_to_figure)
+        except:
+            print('no figure to clean up')
+        
+        write_voltage_data.load_and_plot_data(path_to_data)
+
+        self.assertTrue(os.path.exists(path_to_figure), \
+                'the figure was not created')
